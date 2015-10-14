be4c1a9d0afdc729f884b8f6281ef65ab5879e98
gary9555 <gary9555@gmail.com>
2015-10-15 06:49:37 +0800

    changed sample.cpp to myModel.cpp

--- modelerglobals.h
+++ modelerglobals.h
@@ -5,10 +5,10 @@
 #define M_PI 3.141592653589793238462643383279502
 #endif
 
-// This is a list of the controls for the SampleModel
+// This is a list of the controls for the myModel
 // We'll use these constants to access the values 
 // of the controls from the user interface.
-enum SampleModelControls
+enum myModelControls
 { 
 	XPOS,
 	YPOS,
